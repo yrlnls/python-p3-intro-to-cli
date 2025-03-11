549121f9cedb0733796549e71f18cb8c7960ff25
tillenotuoma <totuoma27@gmail.com>
2025-03-11 21:39:51 +0300

    done

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+def create_grade_report(student_grades):
+    with open('./reports/grade_report.txt', 'w') as gr:
+        for grade in student_grades:
+            gr.write(grade + '\n')
+
 if __name__ == '__main__':
-    pass
+    student_grades = []
+
+    grade = input("Student name, grade: ")
+    while grade:
+        student_grades.append(grade)
+        grade = input("Student name, grade: ")
+
+    create_grade_report(student_grades)
